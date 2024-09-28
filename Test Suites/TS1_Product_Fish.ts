9967a6d7b4e79f7de469558763796392ca458f1a
UC276JC <UC276JC@IN3155587W2.ey.net>
2024-09-28 14:43:06 +0530

    ADDED PRODUCT FISH TC

---
+++ Test Suites/TS1_Product_Fish.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1_Product_Fish</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9df5c84a-af3d-4640-8aa8-fa4127db6583</testSuiteGuid>
+   <testCaseLink>
+      <guid>322fbc3c-0445-4842-a08b-c4de4f0138a2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1_Product_Fish</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
